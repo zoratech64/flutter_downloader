24dbafbb10b785e7e174bdd101d4653b6b509fc7
mac <mac@MacBookPro.mshome.net>
2025-11-08 22:17:25 -0800

    add some configruation for ios notification

--- ios/Classes/FlutterDownloaderPlugin.h
+++ ios/Classes/FlutterDownloaderPlugin.h
@@ -1,13 +1,14 @@
 #import <Flutter/Flutter.h>
+#import <UIKit/UIKit.h>
 
-@interface FlutterDownloaderPlugin : NSObject<FlutterPlugin>
+@interface FlutterDownloaderPlugin : NSObject <FlutterPlugin, UIApplicationDelegate>
 
-@property (nonatomic, copy) void(^backgroundTransferCompletionHandler)(void);
+@property (nonatomic, copy) void (^backgroundTransferCompletionHandler)(void);
+
++ (instancetype)sharedInstance;
 
 + (void)handleNotificationActionPause:(NSString *)taskId;
 + (void)handleNotificationActionResume:(NSString *)taskId;
 + (void)handleNotificationActionCancel:(NSString *)taskId;
-
-+ (instancetype)sharedInstance;
 
 @end
