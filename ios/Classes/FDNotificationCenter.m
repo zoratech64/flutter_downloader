307db2ad27152c1971d582a4633b8517d8cddbea
mac <mac@MacBookPro.mshome.net>
2026-01-11 21:22:51 -0800

    puase download method fixed

--- ios/Classes/FDNotificationCenter.m
+++ ios/Classes/FDNotificationCenter.m
@@ -100,11 +100,8 @@ NSString * const FDActionOpen   = @"FD_ACTION_OPEN";
     if ([category isEqualToString:FDCategoryDone])   identifier = doneId;
 
     NSArray *allIds = @[runningId, pausedId, doneId];
-    NSMutableArray *toRemove = [NSMutableArray arrayWithArray:allIds];
-    [toRemove removeObject:identifier];
-
-    [c removePendingNotificationRequestsWithIdentifiers:toRemove];
-    [c removeDeliveredNotificationsWithIdentifiers:toRemove];
+    [c removePendingNotificationRequestsWithIdentifiers:allIds];
+    [c removeDeliveredNotificationsWithIdentifiers:allIds];
 
     UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
     content.title = title ?: @"Download";
@@ -120,11 +117,15 @@ NSString * const FDActionOpen   = @"FD_ACTION_OPEN";
     }
 
     UNNotificationRequest *request =
-    [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:nil];
-
-    [c addNotificationRequest:request withCompletionHandler:^(NSError *error) {
-        if (error) NSLog(@"[FD] Failed to add/update notification: %@", error);
-    }];
+        [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:nil];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.08 * NSEC_PER_SEC)),
+                   dispatch_get_main_queue(), ^{
+        [c addNotificationRequest:request withCompletionHandler:^(NSError *error) {
+            if (error) {
+                NSLog(@"[FD] Failed to add/update notification: %@", error);
+            }
+        }];
+    });
 }
 
 - (void)removeForTaskId:(NSString *)taskId {
@@ -132,7 +133,6 @@ NSString * const FDActionOpen   = @"FD_ACTION_OPEN";
   NSString *runningId = [NSString stringWithFormat:@"fd.task.%@.running", taskId];
   NSString *pausedId  = [NSString stringWithFormat:@"fd.task.%@.paused",  taskId];
   NSString *doneId    = [NSString stringWithFormat:@"fd.task.%@.done",    taskId];
-
   NSArray *ids = @[runningId, pausedId, doneId];
   [c removePendingNotificationRequestsWithIdentifiers:ids];
   [c removeDeliveredNotificationsWithIdentifiers:ids];
